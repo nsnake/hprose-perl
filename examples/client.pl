1de259e4abdbfcc4cfc560c875dd74e0af6370e9
CGI.NET <root@playcat.net>
2016-02-24 16:28:26 +0800

    perl安装版本

---
+++ examples/client.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+use 5.010;
+use lib qw!../lib!;
+use AnyEvent;
+use Hprose::Client;
+use Data::Dumper qw/Dumper/;
+my $cv     = AE::cv;
+my $client = Hprose::Client->new('http://127.0.0.1/hprosetest');
+   $client->hello( sub { say Dumper @_; $cv->send() } );
+   $cv->recv;
